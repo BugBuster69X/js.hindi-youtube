ec4d348f5d1a7605fec6f32b03353c9a76b386a7
Divyansh Singh <singhdivyansh93044@gmail.com>
2026-07-07 11:20:35 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33 me easy convert ho jata hai 
+// "33abs" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Divyansh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Divyansh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
