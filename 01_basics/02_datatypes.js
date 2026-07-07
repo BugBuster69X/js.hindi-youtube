c5702a58f00ae45dfd2c2b2cecceb1f6312ec422
Divyansh Singh <singhdivyansh93044@gmail.com>
2026-07-07 09:52:12 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+//alert( 3 + 4 ) // we are using node.js not browser
+
+console.log(3 + 6)
+console.log("Divyansh Singh")
+
+let name = "Divyansh" // Ye string hai 
+let age = 18          // Ye number hai 
+let isLoggedIn = true //  boolean type ho gya ye 
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log (typeof null);// iska type aayega object
+console.log(typeof undefined);// iska type aayega undefined hi 
+
+
+
+
